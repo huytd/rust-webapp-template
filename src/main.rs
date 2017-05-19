221d1b39084bd5b53a6c4c87aa093a8295a800c0
Huy <kingbazoka@gmail.com>
2017-05-19 15:09:14 -0700

    Fun with macros

--- src/main.rs
+++ src/main.rs
@@ -4,20 +4,34 @@ extern crate stdweb;
 use stdweb::web::{
     document,
     INode,
-    IEventTarget
+    IEventTarget,
+    Element
 };
 use stdweb::web::event::ClickEvent;
+
+macro_rules! component {
+    ( $name:ident => $expression:block ) => (
+        fn $name() -> Element {
+            $expression
+        }
+    )
+}
+
+component!(green_button => {
+    let button = document().create_element("button");
+    button.set_text_content("Dare to click me?");
+    button.add_event_listener(move |_: ClickEvent| {
+        js! {
+            alert("Yo! This is the Green Button!");
+        }
+    });
+    button
+});
 
 fn main() {
     stdweb::initialize();
 
-    let node = document().create_element("button");
-    node.set_text_content("Click me");
-    node.add_event_listener(move |_: ClickEvent| {
-        js! {
-            alert("Yo!");
-        }
-    });
+    let node = green_button();
     let root = document().query_selector("#root").unwrap();
     root.set_text_content("Hello!");
     root.append_child(&node);
