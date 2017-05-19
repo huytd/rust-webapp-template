2eefe33a9b97a4274eab822236454d3a6c8c2e09
Huy <kingbazoka@gmail.com>
2017-05-19 11:22:37 -0700

    Appending Child example update

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,26 @@
+#[macro_use]
 extern crate stdweb;
 
 use stdweb::web::{
     document,
-    INode
+    INode,
+    IEventTarget
 };
+use stdweb::web::event::ClickEvent;
 
 fn main() {
     stdweb::initialize();
 
+    let node = document().create_element("button");
+    node.set_text_content("Click me");
+    node.add_event_listener(move |_: ClickEvent| {
+        js! {
+            alert("Yo!");
+        }
+    });
     let root = document().query_selector("#root").unwrap();
     root.set_text_content("Hello!");
+    root.append_child(&node);
 
     stdweb::event_loop();
 }
