b2e87b60a5ce5b3dd37e6e4a3278c4d8feabd805
Huy <kingbazoka@gmail.com>
2017-05-21 17:47:00 -0700

    Update component macro
    With the new changes in the component macro, we are now
    able to separate the code logic between component initialize
    and component rendering.
    
    This will give us the ability to call render function
    again in the future, whenever we need to update the component.

--- src/main.rs
+++ src/main.rs
@@ -10,31 +10,61 @@ use stdweb::web::{
 use stdweb::web::event::ClickEvent;
 
 macro_rules! component {
-    ( $name:ident => $expression:block ) => (
-        fn $name() -> Element {
-            $expression
+    ( $name:ident => {
+        init: $init_expression:block,
+        render: $render_expression:expr
+      }
+    ) => (
+        struct $name {
+            element: Element
+        }
+
+        impl $name {
+            pub fn new() -> $name {
+                let e = $name{ element: $init_expression };
+                e
+            }
+
+            pub fn get_element(&self) -> &Element {
+                &self.element
+            }
+
+            pub fn render(&mut self) {
+                $render_expression(&self.element);
+            }
         }
     )
 }
 
-component!(green_button => {
-    let button = document().create_element("button");
-    button.set_text_content("Dare to click me?");
-    button.add_event_listener(move |_: ClickEvent| {
-        js! {
-            alert("Yo! This is the Green Button!");
-        }
-    });
-    button
+macro_rules! mount_component {
+    ( $root:ident, $element:ident) => {
+        $root.append_child($element.get_element());
+        $element.render();
+    }
+}
+
+component!(GreenButton => {
+    init: {
+        let button = document().create_element("button");
+        button.add_event_listener(move |_: ClickEvent| {
+            js! {
+                alert("Yo! This is the Green Button!");
+            }
+        });
+        button
+    },
+    render: |this: &Element| {
+        this.set_text_content("This is a button");
+    }
 });
 
 fn main() {
     stdweb::initialize();
 
-    let node = green_button();
+    let mut button = GreenButton::new();
     let root = document().query_selector("#root").unwrap();
-    root.set_text_content("Hello!");
-    root.append_child(&node);
+
+    mount_component!(root, button);
 
     stdweb::event_loop();
 }
